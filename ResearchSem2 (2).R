94e6fd4c4b0bcae99b07e4b2300eb3ccaf6c4bc8
tleinart <36289461+tleinart@users.noreply.github.com>
2018-06-19 21:18:49 -0400

    Add files via upload
    ResearchSem2 (2).R is the most updated file

--- ResearchSem2 (2).R
+++ ResearchSem2 (2).R
@@ -1,16 +1,4 @@
-install.packages("gsl")
-library("gsl")
-install.packages("f21hyper")
-library("f21hyper")
-
-install.packages("appell")
-
-library("devtools")
-
-install_github("larryleihua/CopulaOne")
-
-##genhypergeo
-
+##Loading in nessecary packages
 library("devtools")
 library("rootSolve")
 library("hypergeo")
@@ -20,15 +8,7 @@ library("foreach")
 library("pryr")
 library("CopulaOne")
 library("appell")
-
-hypergeo_gosper
-
-a <- seq(0.1,1,0.1)
-b <- seq(0.1,1,0.1)
-a <- 0.5
-b <- 0.4
-x <- seq(0.1, 2000, 0.1)
-y <- seq(0.1, 2000, 0.1)
+##Marginal CDF
 OneVarF <- function(a,b,x) {
   hold <- tryCatch(1 - a/(a+b)*Re(hypergeo::hypergeo(1,b,a+b+1,1-x)), error = function(err) FALSE, warning = function(err) FALSE)
   if (!is.logical(hold) && is.finite(hold)) {
@@ -36,6 +16,7 @@ OneVarF <- function(a,b,x) {
   }
   return(NA)
 }
+##Marginal PDF
 OneVarFPDF <- function(a,b,x) {
   hold <- tryCatch((a*b)/(a+b)/(a+b+1)*Re(hypergeo::hypergeo(2,b+1,a+b+2,1-x,tol = 1e-06, maxiter = 10000)), error = function(err) FALSE, warning = function(err) FALSE)
   if (!is.logical(hold) && is.finite(hold)) { 
@@ -43,6 +24,8 @@ OneVarFPDF <- function(a,b,x) {
   }
   return(NA)
 }
+OneVarFPDFVector <- Vectorize(OneVarFPDF,"x")
+##Joint CDF
 TwoVarF <- function(a,b,x1,x2) {
   hold <- tryCatch(OneVarF(a,b,x1)+OneVarF(a,b,x2)-1+((a*(a+1)/((a+b)*(a+b+1)))*Re(appellf1(b,1,1,a+b+2,1-x1,1-x2)$val)), error = function(err) FALSE, warning = function(err) FALSE)
   if (!is.logical(hold) && is.finite(hold)) {
@@ -50,24 +33,17 @@ TwoVarF <- function(a,b,x1,x2) {
   }
   return(NA)
 }
+##Joint PDF
 TwoVarFPDF <- function(a,b,x1,x2) {
-  hold <- tryCatch((((a*b*(a+1)*(b+1))/((a+b)*(a+b+1)*(a+b+2)*(a+b+3)))*Re(appellf1(b+2,2,2,a+b+4,1-x1,1-x2)$val)), error = function(err) FALSE, warning = function(err) FALSE)
-  if (!is.logical(hold) && is.finite(hold)) 
-    return(hold)
+  tem1 <- a*b*(a+1)*(b+1)/(a+b)/(a+b+1)/(a+b+2)/(a+b+3)
+  tem2 <- tryCatch(Re(appell::appellf1(b+2,2,2,a+b+4,1-x1,1-x2,userflag=1)$val),error=function(err) FALSE,warning=function(err) FALSE)
+  if (!is.logical(tem2) && is.finite(tem2)) { 
+    return(tem1*tem2)
+  }
   return(NA)
 }
-
-no_cores <- detectCores()
-registerDoParallel(no_cores)
-c1 <- makeCluster(no_cores)
-clusterEvalQ(c1,library(pryr))
-clusterExport(c1,varlist=c("hypergeo","OneVarF","OneVarFPDF","TwoVarF","TwoVarFPDF","Copula","CopulaPDF","rootFinder","appellf1","a","b","x","y","jpGGEE","jdGGEE","dGGEE_COP"))
-
-stopCluster(c1)
-
-##uniroot,code in copula one,newton raphson method in r
-##Find Copula?
-
+TwoVarFPDFVector <- Vectorize(TwoVarFPDF,c("x1","x2"))
+##Newtons Method / Midpoint Method Root Finder
 rootFinder <- function(a,b,v)
 {
   if (v == 0) {
@@ -89,150 +65,194 @@ rootFinder <- function(a,b,v)
       }
       DEN <- OneVarFPDF(a,b,exp(t)) * exp(t)
       CDF <- OneVarF(a,b,exp(t))
+      if(is.na(DEN)) {
+        return(NA)
+      }
+      if(is.na(CDF)) {
+        return(NA)
+      }
       if (CDF < v) {
         lower <- t
       }
       else {
         upper <- t
       }
-      if(is.na(DEN)) {
-        return(NA)
-      }
     }
     out <- exp(t)
   }
   return(out)
 }
-
-##empiricalCDF - DONE
-##Copula - DONE
-##CopulaPDF - DONE
-##MLE
-##Cores And Multithreading
-
+##Cubic Spline Interpolation NOT FULLY IMPLEMENTED YET
+rootFinderInterp <- function(a,b,v) {
+  minval <- min(v)
+  maxval <- max(v)
+  testvals <- seq(minval,maxval,(maxval-minval)/199)
+  basevals <- sapply(testvals,rootFinder,a=a,b=b)
+  testing <- matrix(c(testvals,basevals),ncol = 2)
+  return(spline(x = testvals,y = basevals,xout = v,method = "hyman")$y)
+}
+##Empirical CDF (uniroot appears to be better)
 empiricalCDF <- function(v) {
   return(rank(v)/(length(v)+1))
 }
-
+##Copula CDF
 Copula <- function(a,b,v1,v2) {
   x1 <- rootFinder(a,b,v1)
   x2 <- rootFinder(a,b,v2)
   return(TwoVarF(a,b,x1,x2))
 }
-
+##Copula PDF
 CopulaPDF <- function(a,b,v1,v2) {
-  x1 <- rootFinder(a,b,v1)
-  x2 <- rootFinder(a,b,v2)
-  c1 <- OneVarFPDF(a,b,x1)
-  c2 <- OneVarFPDF(a,b,x2)
-  return(TwoVarFPDF(a,b,x1,x2)/c1/c2)
+  q1 <- tryCatch(rootFinder(a,b,v1), error = function(err) FALSE, warning = function(err) FALSE)
+  q2 <- tryCatch(rootFinder(a,b,v2), error = function(err) FALSE, warning = function(err) FALSE)
+  if (is.logical(q1)||is.logical(q2))
+  {
+    return(NA)
+  } 
+  else {
+    if (is.finite(q1) && is.finite(q2))
+    {
+      tem1 <- TwoVarFPDF(a,b,q1,q2)
+      tem2 <- OneVarFPDF(a,b,q1)
+      tem3 <- OneVarFPDF(a,b,q2)
+      if (is.finite(tem1) && is.finite(tem2) && is.finite(tem3))
+      {
+        return(tem1/tem2/tem3)
+      } else
+      {
+        return(NA)
+      }
+    } else
+    {
+      return(NA)
+    }
+  }
 }
-
-MLE <- function(a,b,m) {
-  hold <- parRapply(c1,m,function(x) {-log(CopulaPDF(a,b,x[1],x[2]))})
+##Allows Copula PDF to take a vector as input
+CopulaPDFVector <- Vectorize(CopulaPDF,c("v1","v2"))
+##Portion of the MLE run on a specific core
+MLEPartial <- function(a,b,m,i) {
+  x <- floor(nrow(m)/7)
+  if(i < 7) {
+    ourdat <- m[(((i-1)*x+1)):(i*x),]
+  }
+  else {
+    ourdat <- m[(((i-1)*x)+1):(nrow(m)),]
+  }
+  hold <- CopulaPDFVector(a,b,ourdat[,1],ourdat[,2])
+  return(-log(hold))
+}
+##Portion of the MLE that assigns portions of the computations to the cores
+MLEFinal <- function(a,b,m) {
+  hold <- unlist(parLapply(c1,1:7,MLEPartial,a=a,b=b,m=m))
   hold <- hold[!is.na(hold)]
   hold <- hold[is.finite(hold)]
   hold <- hold[!is.nan(hold)]
   return(sum(hold))
 }
 
-solveMLE <- function(m) {
+##Coputes the actual MLE using the optim function
+solveMLE <- function(m,a,b) {
   hold <-  m
   corr <- cor(x=hold[,1],y=hold[,2],method="kendall")
   if(corr < 0) {
     hold[,2] <- 1-hold[,2]
   }
-  matrix1 <<-  hold
-  temp <- function(a) {
-    return(MLE(a[1],a[2],matrix1))
+  matrix1 <-  hold
+  temp <- function(c) {
+    return(MLEFinal(c[1],c[2],matrix1))
   }
-  return(optim(c(0.5,0.5),temp,method="L-BFGS-B", lower = 0.1, upper = 1-1e-6))
+  no_cores <- detectCores() - 1
+  registerDoParallel(no_cores)
+  c1 <<- makeCluster(no_cores)
+  clusterEvalQ(c1,library(pryr))
+  clusterExport(c1,varlist=c("hypergeo","OneVarF","OneVarFPDF","OneVarFPDFVector","TwoVarF","TwoVarFPDF","TwoVarFPDFVector","Copula","CopulaPDF","CopulaPDFVector","rootFinder","rootFinderInterp","appellf1","jpGGEE","jdGGEE","dGGEE_COP","qGGEE","dGGEE","dat"))
+  answer <- (optim(c(a,b),temp,method="L-BFGS-B",lower = 0.1, upper = 5))
+  stopCluster(c1)
+  return(answer)
+}
+##Coputes the actual MLE using the NLM function
+solveMLENLM <- function(m,a,b) {
+  hold <-  m
+  corr <- cor(x=hold[,1],y=hold[,2],method="kendall")
+  if(corr < 0) {
+    hold[,2] <- 1-hold[,2]
+  }
+  matrix1 <-  hold
+  temp <- function(c) {
+    return(MLEFinal(exp(c[1])+0.01,exp(c[2])+0.01,matrix1))
+  }
+  no_cores <- detectCores() - 1
+  registerDoParallel(no_cores)
+  c1 <<- makeCluster(no_cores)
+  clusterEvalQ(c1,library(pryr))
+  clusterExport(c1,varlist=c("hypergeo","OneVarF","OneVarFPDF","OneVarFPDFVector","TwoVarF","TwoVarFPDF","TwoVarFPDFVector","Copula","CopulaPDF","CopulaPDFVector","rootFinder","rootFinderInterp","appellf1","jpGGEE","jdGGEE","dGGEE_COP","qGGEE","dGGEE","dat"))
+  answer <- (nlm(temp,c(log(a-0.01),log(b-0.01))))
+  stopCluster(c1)
+  answer$estimate <- exp(answer$estimate)+0.01
+  return(answer)
 }
 
-##CopulaOne Empirical cdf - DONE
-##Time TwoVarPDF and TwoVarF against CopulaOne - DONE
-##Remove loop when listing data in MLE -LEAVE
-##Test against CopulaOne data
-##Test against different methods in optim
-
-a <- seq(0.1,0.9,0.1)
-b <- seq(0.1,0.9,0.1)
-x <- seq(0.1,200,0.1)
-y <- 1
-
-system.time(parSapply(c1,x,function(x) {mapply(function(a,b) TwoVarF(a,b,x,1),a,b)}))
-system.time(parSapply(c1,x,function(x) {mapply(function(a,b) jpGGEE(a,b,x,1),a,b)}))
-
-system.time(parSapply(c1,x,function(x) {mapply(function(a,b) TwoVarFPDF(a,b,x,1),a,b)}))
-system.time(parSapply(c1,x,function(x) {mapply(function(a,b) jdGGEE(a,b,x,1),a,b)}))
-
-system.time(parSapply(c1,x,function(x) {mapply(function(a,b) CopulaPDF(a,b,x,1),a,b)}))
-system.time(parSapply(c1,x,function(x) {mapply(function(a,b) dGGEE_COP(a,b,x,1),a,b)}))
+################################
+## Work in Progress / Testing ##
+################################
 
 testData <- read.table("CopulaOneData.txt")
-
-system.time(empiricalCDF(testData$V3))
-
-system.time(uscore(testData$V3))
-
-MLE(0.2,0.8,testData$V3,testData$V4)
-
-dGGEE_COP(0.2,0.2,0.8,0.8)
-CopulaPDF(0.8,0.8,0.2,0.2)
-
-u1 <- empiricalCDF(testData$V3)
-u2 <- empiricalCDF(testData$V4)
-
-hold <- rbind(u1,u2)
-hold
-
-testData$V3
-
-empiricalCDF(testData$V4)
-
-par0 <- c(0.3,0.7)
-patternpar = seq(1,2)
-
-uscore(testData$v3)
-
-fitCopulaOne(c(0.5,0.5),dat = hold,copula_family = "GGEE")
-fitCopulaOne(par0 = par0, whichpar=seq(1,length(par0)), patternpar=seq(1,2), dat = hold, flag=1, integration=F, opt="L-BFGS-B", se=F, lower=rep(0.1, length(unique(patternpar))), upper=rep(5, length(unique(patternpar))), trace=0, factr=1e9, printlevel=0, copula_family="GGEE")
-
-solveMLE(testData$V3,testData$V4)
-
-##Find why our TwoVarPDF and CDF are faster
-##Get Copula One working in seperate R file
-##Compare to solve MLE
-data("euro0306")
-dat <- matrix(c(testData$V2,testData$V3),ncol=2)
-dat <- uscore(dat)
-
-fitCopulaOne(c(0.5,0.5),dat = dat,copula_family = "GGEE",lower = 0.1,upper = 1-1e-6)
-solveMLE(testData$V2,testData$V3)
 
 data("euro0306")
 dat <- uscore(euro0306[,c(2,3)])[1:100,]
 dat <- matrix(c(uscore(testData$V2),uscore(testData$V3)),ncol = 2)
 par0 <- c(0.5, 0.5)
 
-system.time(cat(solveMLE(dat)$par))
-system.time(cat(fitCopulaOne(par0, dat=dat, copula_family="GGEE",lower = 0.1, upper = 1-1e-6)$par))
+system.time(cat(solveMLE(dat,0.3,0.3)$par))
 
-testData$V5
+system.time(cat(fitCopulaOne(par0, dat=dat, copula_family="GGEE",lower = 0.1, upper = 5,opt="optim")$par))
 
-MLE(0.511,0.65,dat[,1],dat[,2])
+fitCopulaOne(par0, dat=dat, copula_family="GGEE",lower = 0.1, upper = 5,opt="nlm")
 
-dGGEE_COP(0.2,0.4,1.2,0.2)
-CopulaPDF(1.2,0.2,0.2,0.4)
+solveMLE(dat)
 
-
-## Implent COpula Removal of NA values
-## Get parallel computing working better
-## Compare ranking methods for NA values
-
-##Computational Speed
-##Overleaf
+stopCluster(c1)
 
 rm(list = ls())
 
 ##Interpelation
+
+##Simulation
+
+a <- 0.3
+b <- 0.4
+
+set.seed(1000)
+r1 <- rgamma(1000, shape = a, scale = 1)
+
+##set.seed(3000)
+r2 <- rgamma(1000, shape = b, scale = 1)
+
+##set.seed(5000)
+e11 <- rexp(1000,1)
+
+##set.seed(7000)
+e12 <- rexp(1000,1)
+
+##set.seed(9000)
+e21 <- rexp(1000,1)
+
+##set.seed(11000)
+e22 <- rexp(1000,1)
+
+x1 <- (r1/r2)*(e11/e12)
+x2 <- (r1/r2)*(e21/e22)
+
+dat <- matrix(c(uscore(x1),uscore(x2)),ncol = 2)
+system.time(solveMLENLM(dat,0.3,0.3))
+range(x1,x2)
+
+##Try different starting functions to see if we get better estimation on optim and nlm
+##Find out which is faster, optim or nlm
+##Clean the code, add comments, push to github
+##Try different models - DONE
+##Cubic Spline Interpolation - DONE
+
+
+##Implement Cubic Spline into main file
